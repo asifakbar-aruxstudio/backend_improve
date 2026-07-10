893240068c4301fb5af29900e63e9e78542f254b
Asif Akbar <786asifakbar53@gmail.com>
2026-07-10 20:40:51 +0500

    Add image upload functionality and response handling in user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../utills/asyncHandler.js";
 import { ApiError } from "../utills/ApiError.js";   
-
+import {uploadToCloudinary} from "../utills/cloudinary.js";
+import { ApiResponse } from "../utills/ApiResponse.js";
 
 const registerUser = asyncHandler(async(req, res) => {
   
@@ -31,9 +32,42 @@ const registerUser = asyncHandler(async(req, res) => {
    const avatarLocalPath = req.files?.avatar[0]?.path;
    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-   
+   if(!avatarLocalPath || !coverImageLocalPath){
+    throw new ApiError(400, "Avatar and cover image are required");
+   }    
+
+   const avatar = await uploadToCloudinary(avatarLocalPath, "avatar");
+   const coverImage = await uploadToCloudinary(coverImageLocalPath, "coverImage");
+
+
+   if(!avatar || !coverImage){
+    throw new ApiError(500, "Failed to upload images");
+   }    
+
+   User.create({
+    fullName,
+    username: username.toLowerCase(),
+    email,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage.url
+
+})
+
+    const createdUser = await User.findById(User._id).select(
+        "-password -accessToken"
+    )
+        if(!createdUser){
+            throw new ApiError(500, "User not found after creation");
+        } 
+        
+        return res.status(201).json({
+            new ApiResponse(200, createdUser, "User registered successfully")
+        });
+
 });   
-    
+
+
 
 
 export { registerUser };
