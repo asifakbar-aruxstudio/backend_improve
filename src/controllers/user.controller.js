173ab3a29119e7c3705685c7abb4c01c143bf687
Asif Akbar <786asifakbar53@gmail.com>
2026-07-10 20:23:26 +0500

    Implement user registration validation and file path extraction

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,6 +21,17 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
+   const ExistUser = await User.findone({
+    $or: [{ email }, { username }],
+   })
+   if(ExistUser){
+    throw new ApiError(409, "User already exists");
+   }    
+
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   
 });   
     
 
