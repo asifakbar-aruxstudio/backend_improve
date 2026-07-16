6ffcbc8f90e8fa85fe02e2b90954ae758b21cb61
Asif Akbar <786asifakbar53@gmail.com>
2026-07-16 23:21:42 +0500

    Fix formatting in app.js for express.json() options

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 }));
 
 
-app.use(express.json({limit : "50mb"}));
+app.use(express.json({ limit : "50mb" }));
 app.use(express.urlencoded({limit : "50mb", extended: true}));
 app.use(express.static("public"));
 app.use(cookieParser());
